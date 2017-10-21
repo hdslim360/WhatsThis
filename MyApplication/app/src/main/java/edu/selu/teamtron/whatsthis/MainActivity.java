991c3da70ac67ae11b524386f507af441dad0e34
Larayo <justin.laurent@selu.edu>
2017-10-21 13:19:43 -0500

    improved view pager

--- MyApplication/app/src/main/java/edu/selu/teamtron/whatsthis/MainActivity.java
+++ MyApplication/app/src/main/java/edu/selu/teamtron/whatsthis/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.selu.teamtron.whatsthis;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,10 +15,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View background = findViewById(R.id.am_background_view);
+        final View background = findViewById(R.id.am_background_view);
         ViewPager viewPager = (ViewPager) findViewById(R.id.am_view_pager);
         MainPagerAdapter adapter = new MainPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
 
+        final int colorBlue = ContextCompat.getColor(this, R.color.light_blue);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if(position == 0) {
+                    background.setBackgroundColor(colorBlue);
+                    background.setAlpha(1 - positionOffset);
+                }
+                else if(position == 1) {
+                    background.setBackgroundColor(colorBlue);
+                    background.setAlpha(positionOffset);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+
+        });
     }
 }
