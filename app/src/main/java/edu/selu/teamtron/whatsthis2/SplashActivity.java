b72cc35ea9720625463158f7915b4b0599d4b242
Larayo <justin.laurent@selu.edu>
2017-11-07 01:21:06 -0600

    created splash framework

---
+++ app/src/main/java/edu/selu/teamtron/whatsthis2/SplashActivity.java
@@ -0,0 +1,26 @@
+package edu.selu.teamtron.whatsthis2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
