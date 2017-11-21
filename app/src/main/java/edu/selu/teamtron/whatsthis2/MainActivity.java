5cb2a1404eb2a17e3d461161e30433e0e7f3ffe4
hdslim360 <jeremy.slimmer@selu.edu>
2017-11-20 18:47:28 -0600

    changed toolbar variable

--- app/src/main/java/edu/selu/teamtron/whatsthis2/MainActivity.java
+++ app/src/main/java/edu/selu/teamtron/whatsthis2/MainActivity.java
@@ -70,7 +70,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar;
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
