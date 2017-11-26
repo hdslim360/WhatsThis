7245fdac59c1d94607a56b7bdb5826625b482954
chadlafontaine <chad.lafontaine@selu.edu>
2017-11-25 19:31:10 -0600

    I removed the image view that I was using to see the screen shot that I had captured from the layout. Still stuck on getting the image to the file path

--- app/src/main/java/edu/selu/teamtron/whatsthis2/SplashActivity.java
+++ app/src/main/java/edu/selu/teamtron/whatsthis2/SplashActivity.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class SplashScreen extends Activity {
+public class SplashActivity extends Activity {
 
     private final int SPLASH_DISPLAY_LENGTH = 3000;
 
@@ -17,9 +17,9 @@ public class SplashScreen extends Activity {
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
-                SplashScreen.this.startActivity(mainIntent);
-                SplashScreen.this.finish();
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
     }
